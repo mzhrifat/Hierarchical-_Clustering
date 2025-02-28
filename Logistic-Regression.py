f67a53378727e6918eaf47ee244fbd87c95a9916
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-02-28 22:50:31 +0600

    grid search commit

--- Logistic-Regression.py
+++ Logistic-Regression.py
@@ -11,20 +11,42 @@ logr.fit(x,y)
 
 predicted=logr.predict(np.array([3.46]).reshape(-1,1))
 print(predicted)
-"""
+
 
 #Coefficient
-import numpy
+
+import numpy as np
+from pandas.core.interchange.from_dataframe import primitive_column_to_ndarray
 from sklearn import linear_model
 
 #Reshaped for Logistic function.
-X = numpy.array([3.78, 2.44, 2.09, 0.14, 1.72, 1.65, 4.92, 4.37, 4.96, 4.52, 3.69, 5.88]).reshape(-1,1)
-y = numpy.array([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
+X = np.array([3.78, 2.44, 2.09, 0.14, 1.72, 1.65, 4.92, 4.37, 4.96, 4.52, 3.69, 5.88]).reshape(-1,1)
+y = np.array([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
 
-logr = linear_model.LogisticRegression()
+logr=linear_model.LogisticRegression()
 logr.fit(X,y)
 
-log_odds = logr.coef_
-odds = numpy.exp(log_odds)
-
+log_odds=logr.coef_
+odds=np.exp(log_odds)
 print(odds)
+"""
+
+#Function Explained
+
+import numpy as np
+from sklearn import linear_model
+
+X = np.array([3.78, 2.44, 2.09, 0.14, 1.72, 1.65, 4.92, 4.37, 4.96, 4.52, 3.69, 5.88]).reshape(-1,1)
+y = np.array([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1])
+
+
+logr=linear_model.LogisticRegression()
+logr.fit(X,y)
+
+def logit2prob(logr,X):
+    log_odds=logr.coef_ *X + logr.intercept_
+    odds=np.exp(log_odds)
+    probability=odds/(1+odds)
+    return (probability)
+
+print(logit2prob(logr, X))

992e408f8d97c6355f04035d339304051dece1e4
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-03-02 01:43:04 +0600

    Elbow method commit

--- K-means.py
+++ K-means.py
@@ -1,8 +1,31 @@
-
+"""
 import matplotlib.pyplot as plt
+from scipy.cluster.vq import kmeans
 
 x = [4, 5, 10, 4, 3, 11, 14 , 6, 10, 12]
 y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
 
 plt.scatter(x,y)
+plt.show()
+"""
+
+#Now we utilize the elbow method to visualize the intertia for different values of K:
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+x = [4, 5, 10, 4, 3, 11, 14 , 6, 10, 12]
+y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
+
+data = list(zip(x, y))
+inertias = []
+
+for i in range(1,11):
+    kmeans = KMeans(n_clusters=i)
+    kmeans.fit(data)
+    inertias.append(kmeans.inertia_)
+
+plt.plot(range(1,11), inertias, marker='o')
+plt.title('Elbow method',color='green')
+plt.xlabel('Number of clusters',color='r')
+plt.ylabel('Inertia')
 plt.show()

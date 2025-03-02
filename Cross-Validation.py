6eda055e886d3eca6472b316ffa8aa50a8461f02
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-03-03 01:19:46 +0600

    Leave-p-out commit

--- Cross-Validation.py
+++ Cross-Validation.py
@@ -62,7 +62,7 @@ scores = cross_val_score(clf, x, y, cv=sk_folds)
 print("Cross Validation Scores: ", scores)
 print("Average CV Score: ", scores.mean())
 print("Number of CV Scores used in Average: ", len(scores))
-"""
+
 
 
 #Leave-One-Out (LOO) cross validation
@@ -82,4 +82,22 @@ print("Cross Validation Scores: ", scores)
 print("Average CV Score: ", scores.mean())
 print("Number of CV Scores used in Average: ", len(scores))
 
+"""
+#Leave-p-out
 
+from sklearn import datasets
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import LeavePOut,cross_val_score
+
+X,y=datasets.load_iris(return_X_y=True)
+
+clf=DecisionTreeClassifier(random_state=42)
+
+lpo=LeavePOut(p=2)
+
+scores=cross_val_score(clf,X,y,cv=lpo)
+
+
+print("Cross Validation Scores: ", scores)
+print("Average CV Score: ", scores.mean())
+print("Number of CV Scores used in Average: ", len(scores))

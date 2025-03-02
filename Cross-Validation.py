e2447c39b20c7e025ca0399e3e8391172fe23e90
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2025-03-03 00:50:03 +0600

    k-fold  Cross validation commit

--- Cross-Validation.py
+++ Cross-Validation.py
@@ -18,7 +18,7 @@ scores = cross_val_score(model, X, y, cv=kf)
 # রেজাল্ট প্রিন্ট করা
 print("Cross-validation scores:", scores)
 print("Mean accuracy:", scores.mean())
-"""
+
 
 #Run k-fold for cv
 
@@ -37,3 +37,29 @@ scores=cross_val_score(clf,X,y ,cv=k_folds)
 print("Croos Validation Scores:",scores)
 print("Average CV Score:",scores.mean())
 print("Number of CV SCores used in Average:",len(scores))
+"""
+
+#k-Fold Cross Validation
+
+from sklearn import datasets
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import StratifiedKFold, cross_val_score
+
+# Iris ডেটাসেট লোড করা
+x,y = datasets.load_iris(return_X_y=True)
+
+#Decision Tree model make
+
+clf=DecisionTreeClassifier(random_state=42)
+
+#Startified k-fold cross validation setup
+sk_folds = StratifiedKFold(n_splits=5)
+
+# মডেল মূল্যায়ন করা
+scores = cross_val_score(clf, x, y, cv=sk_folds)
+
+# ফলাফল প্রিন্ট করা
+print("Cross Validation Scores: ", scores)
+print("Average CV Score: ", scores.mean())
+print("Number of CV Scores used in Average: ", len(scores))
+
